7bf7a8f31e27e52858f67cb53a22a11b806c5a0a
Thomas More <yitzhak@lucy.villette>
2021-03-25 15:45:30 -0400

    version 1.0

---
+++ app/src/test/java/com/github/tmo1/sms_ie/ExampleUnitTest.kt
@@ -0,0 +1,36 @@
+/*
+ * SMS Import / Export: a simple Android app for importing and exporting SMS messages from and to JSON files.
+ * Copyright (c) 2021 Thomas More
+ *
+ * This file is part of SMS Import / Export.
+ * Foobar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.sms_ie
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
