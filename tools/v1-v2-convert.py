3152258e3cba2cfbb40212bcf939dc971001ed84
Thomas More <tmore1@gmx.com>
2023-07-20 15:13:44 -0400

    [messages] Add shebang and correct copyright

--- tools/v1-v2-convert.py
+++ tools/v1-v2-convert.py
@@ -1,7 +1,9 @@
+#! /usr/bin/python3
+
 # SMS Import / Export: a simple Android app for importing and exporting SMS and MMS messages,
 # call logs, and contacts, from and to JSON / NDJSON files.
 #
-# Copyright (c) 2021-2023 Thomas More
+# Copyright (c) 2023 Thomas More
 #
 # This file is part of SMS Import / Export.
 #
